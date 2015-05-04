1c99234820431b295a4b98517170ad59a1b849c4
Milan <milan.panik1@gmail.com>
2015-05-04 15:38:57 +0200

    REST impl

--- rest/src/main/java/cz/fi/muni/pb138/broker/web/rest/PropertyResource.java
+++ rest/src/main/java/cz/fi/muni/pb138/broker/web/rest/PropertyResource.java
@@ -1,7 +1,13 @@
 package cz.fi.muni.pb138.broker.web.rest;
 
+import cz.fi.muni.pb138.broker.business.service.PropertyService;
+import cz.fi.muni.pb138.broker.data.model.Property;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 /**
  * @author Milan
@@ -10,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/properties")
 public class PropertyResource {
 
+    @Inject
+    private PropertyService propertyService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Resources<Property> findAll() {
+        return new Resources<>(propertyService.findAll());
+    }
 }
