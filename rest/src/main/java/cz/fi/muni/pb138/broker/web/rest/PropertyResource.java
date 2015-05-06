f9099251e9c8585fdecbf00fc763a4732d5d9036
Milan <milan.panik1@gmail.com>
2015-05-06 14:36:45 +0200

    clean up

--- rest/src/main/java/cz/fi/muni/pb138/broker/web/rest/PropertyResource.java
+++ rest/src/main/java/cz/fi/muni/pb138/broker/web/rest/PropertyResource.java
@@ -23,8 +23,4 @@ public class PropertyResource {
     public Resources<Property> findAll() {
         return new Resources<>(propertyService.findAll());
     }
-    @RequestMapping(value = "/oneprop/@id",method = RequestMethod.GET)
-    public Property findOne(Long id) {
-        return propertyService.findOne(id);
-    }
 }
