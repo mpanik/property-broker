c6d31b644b6ff20e5713b515ef37dced86a99d09
xMartinn <martinpipis@gmail.com>
2015-05-11 00:12:30 +0200

    added avgPrice controller

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -37,3 +37,26 @@ angular.module('propertyBrokerApp.controllers').
 
 
     });
+
+
+
+angular.module('propertyBrokerApp.controllers',[]).
+    controller('AvgPriceCrtl',['$scope',function($scope){
+
+        var properties = [
+            {Area: 33, Price: 1440000, Type: 'ONE_KK', Street: 'Kubíkova', District: 'Líšeň', City: 'Brno'},
+            {Area: 82, Price: 3190000, Type: 'THREE_ONE', Street: 'Bayerova', District: 'Veveří', City: 'Brno'},
+            {Area: 88, Price: 2900000, Type: 'FOUR_KK', Street: 'Vinohrady', District: 'Vinohrady', City: 'Brno'},
+            {Area: 59, Price: 2755113, Type: 'TWO_KK', Street: 'Kníničky', District: 'Kníničky', City: 'Brno'},
+            {Area: 171, Price: 7400000, Type: 'THREE_ONE', Street: 'Mojžíšova', District: 'Královo pole', City: 'Brno'},
+            {Area: 96, Price: 4100000, Type: 'THREE_KK', Street: 'Kotlanova', District: 'Líšeň', City: 'Brno'},
+            {Area: 67, Price: 2259000, Type: 'TWO_KK', Street: 'Bělohorská', District: 'Židenice', City: 'Brno'}];
+
+        var totalPrice=0;
+        properties.forEach(function (property) {
+                totalPrice=totalPrice+parseInt(property.Price);
+        });
+        var avgPrice=Math.round(totalPrice/properties.length);
+        $scope.avgPrice=avgPrice;
+    }]);
+
