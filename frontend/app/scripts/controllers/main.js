13945e9b338a184c718ae286f0b88065cd9b5b46
VitaNuova <vitanuova0512@gmail.com>
2015-05-13 23:02:26 +0200

    Added function for creating markers to show on map

--- frontend/app/scripts/controllers/main.js
+++ frontend/app/scripts/controllers/main.js
@@ -7,33 +7,18 @@ angular.module('propertyBrokerApp.controllers').
             $scope.map = { center: { latitude: 49.2044255, longitude: 16.6325489 }, zoom: 12 };
         });
 
-
-        $scope.properties = [{Area: 33, Price: 1440000, Type: 'ONE_KK', Street: 'Kubíkova', District: 'Líšeň', City: 'Brno'},
-                          {Area: 82, Price: 3190000, Type: 'THREE_ONE', Street: 'Bayerova', District: 'Veveří', City: 'Brno'},
-                          {Area: 88, Price: 2900000, Type: 'FOUR_KK', Street: 'Vinohrady', District: 'Vinohrady', City: 'Brno'},
-                          {Area: 59, Price: 2755113, Type: 'TWO_KK', Street: 'Kníničky', District: 'Kníničky', City: 'Brno'},
-                          {Area: 171, Price: 7400000, Type: 'THREE_ONE', Street: 'Mojžíšova', District: 'Královo pole', City: 'Brno'},
-                          {Area: 96, Price: 4100000, Type: 'THREE_KK', Street: 'Kotlanova', District: 'Líšeň', City: 'Brno'},
-                          {Area: 67, Price: 2259000, Type: 'TWO_KK', Street: 'Bělohorská', District: 'Židenice', City: 'Brno'}];
-
         $scope.markers = [];
-        var id = 0;
 
-        function getCoords(url) {
-            var resource = $resource(url, {});
-            resource.get(function (data) {
-                var latitude = data.results[0].geometry.location.lat;
-                var longitude = data.results[0].geometry.location.lng;
-                var marker = {id: id, coords: {latitude: latitude, longitude: longitude}};
-                id++;
-                $scope.markers.push(marker);
-            });
-    }
+        /*$scope.properties = [{Area: 33, Price: 1440000, Type: 'ONE_KK', Street: 'Kubíkova', District: 'Líšeň', City: 'Brno', x: 49.20282, y: 16.564736},
+                          {Area: 82, Price: 3190000, Type: 'THREE_ONE', Street: 'Bayerova', District: 'Veveří', City: 'Brno', x: 49.2299654, y: 16.593883},
+                          {Area: 88, Price: 2900000, Type: 'FOUR_KK', Street: 'Vinohrady', District: 'Vinohrady', City: 'Brno', x: 49.225979, y: 16.555563},
+                          {Area: 59, Price: 2755113, Type: 'TWO_KK', Street: 'Kníničky', District: 'Kníničky', City: 'Brno', x: 49.2007647, y: 16.6058647},
+                          {Area: 171, Price: 7400000, Type: 'THREE_ONE', Street: 'Mojžíšova', District: 'Královo pole', City: 'Brno', x: 49.1808461, y: 16.598696},
+                          {Area: 96, Price: 4100000, Type: 'THREE_KK', Street: 'Kotlanova', District: 'Líšeň', City: 'Brno', x: 49.228787, y: 16.5260415},
+                          {Area: 67, Price: 2259000, Type: 'TWO_KK', Street: 'Bělohorská', District: 'Židenice', City: 'Brno', x: 49.191932, y: 16.618645}];*/
 
-        for(var i = 0; i < $scope.properties.length; i++) {
-            var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + $scope.properties[i].City + '+' + $scope.properties[i].Street;
-            getCoords(url);
-        }
+
+
 
     });
 
--- frontend/app/scripts/directives/search-directive/search-directive.js
+++ frontend/app/scripts/directives/search-directive/search-directive.js
@@ -12,6 +12,7 @@ angular.module('propertyBrokerApp.directives')
 
                 $scope.propertySelected = function(selected) {
                     console.log(selected);
+                    //$scope.markers = buildMarkersArray($scope.propertySelected);
                     //TODO: better info (not alert) + show on map (with different colored marker), when unselected remove marker from map
                 };
 
@@ -22,3 +23,16 @@ angular.module('propertyBrokerApp.directives')
             }]
         };
     });
+
+function buildMarkersArray(properties) {
+    var markers = [];
+    var id = 0;
+    for(var i = 0; i < properties.length; i++) {
+        var latitude = properties[i].x;
+        var longitude = properties[i].y;
+        var marker = {id: id, coords: {latitude: latitude, longitude: longitude}};
+        id++;
+        markers.push(marker);
+    }
+    return markers;
+}
