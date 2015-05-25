cbd7c8f7ad9adf7b34b87ebee793d16c6efa6b52
Milan <milan.panik1@gmail.com>
2015-05-25 23:06:33 +0200

    integrated ui-router

--- frontend/app/scripts/routes/routes.js
+++ frontend/app/scripts/routes/routes.js
@@ -1,20 +1,21 @@
 'use strict';
 
 angular.module('propertyBrokerApp.routes')
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/', {
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl'
             })
-            .when('/stats', {
+            .state('stats', {
+                url: '/stats',
                 templateUrl: 'views/stats.html',
                 controller: 'StatsCtrl'
-            })
-            .when('/about', {
-                templateUrl: 'views/about.html'
-            })
-            .otherwise({
-                redirectTo: '/'
             });
-    });
+
+    }]
+);
