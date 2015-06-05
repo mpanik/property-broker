a481aeb6c195a6bb490afd775b027a59b15920d5
Milan <milan.panik1@gmail.com>
2015-06-05 22:19:54 +0200

    statistics refactored to use lodash functions

--- frontend/app/scripts/controllers/stats.js
+++ frontend/app/scripts/controllers/stats.js
@@ -1,17 +1,111 @@
 'use strict';
 
 angular.module('propertyBrokerApp.controllers').
-    controller('StatsCtrl', ['$scope', 'growl', 'PropertyService',
-        function ($scope, growl, PropertyService) {
+    controller('StatsCtrl', ['$scope', 'PropertyService', 'lodash',
+        function ($scope, PropertyService, _) {
 
             PropertyService.findAll(
-                function(data) {
+                function (data) {
                     $scope.properties = data;
-                },
-                function() {
-                    growl.error('Failed to connect to server. Please try again later.');
+                    $scope.districtsStats = calculateDistrictsStats($scope.properties);
                 }
             );
+
+            function divideValues(item1, item2) {
+                return Math.round(parseInt(item1) / parseInt(item2));
+            }
+
+            function parseIntAndGetMaxPrice(item1, item2) {
+                return Math.max(parseInt(item1.price), parseInt(item2.price)) === parseInt(item1.price) ? item1 : item2;
+            }
+
+            function parseIntAndGetMinPrice(item1, item2) {
+                return Math.min(parseInt(item1.price), parseInt(item2.price)) === parseInt(item1.price) ? item1 : item2;
+            }
+
+            function extractDistrictsStatsFrom(properties) {
+                var statsByDistrict = [];
+
+                function updateAndSumStats(objectToBeUpdated, property) {
+                    objectToBeUpdated.sumPrices += property.price;
+                    objectToBeUpdated.sumArea += property.area;
+                    objectToBeUpdated.count++;
+                    objectToBeUpdated.mostExpensive = parseIntAndGetMaxPrice(objectToBeUpdated.mostExpensive, property);
+                    objectToBeUpdated.cheapest = parseIntAndGetMinPrice(objectToBeUpdated.cheapest, property);
+                }
+
+                function initStreetsArray(property) {
+                    if (statsByDistrict[property.address.district].streets === undefined) {
+                        statsByDistrict[property.address.district].streets = [];
+                    }
+                    statsByDistrict[property.address.district].streets[property.address.street] = initStats(property);
+                }
+
+                function initStats(property) {
+                    return {
+                        sumPrices: property.price,
+                        sumArea: property.area,
+                        count: 1,
+                        mostExpensive: property,
+                        cheapest: property
+                    };
+                }
+
+                function updateStreetStats(property) {
+                    if (_.has(statsByDistrict[property.address.district].streets, property.address.street)) {
+                        updateAndSumStats(statsByDistrict[property.address.district].streets[property.address.street], property);
+                    } else {
+                        initStreetsArray(property);
+                    }
+                }
+
+                _.forEach(properties, function (property) {
+                    if (_.has(statsByDistrict, property.address.district)) {
+                        updateAndSumStats(statsByDistrict[property.address.district], property);
+                        updateStreetStats(property);
+                    } else {
+                        statsByDistrict[property.address.district] = initStats(property);
+                        initStreetsArray(property);
+                    }
+                });
+
+                return statsByDistrict;
+            }
+
+            function calculateDistrictsStats(properties) {
+                var districts = extractDistrictsStatsFrom(properties);
+                var districtsStats = {};
+
+                function makeStats(object) {
+                    return {
+                        avgPriceMSquare: divideValues(object.sumPrices, object.sumArea),
+                        count: object.count,
+                        avgPrice: divideValues(object.sumPrices, object.count),
+                        mostExpensive: object.mostExpensive,
+                        cheapest: object.cheapest
+                    };
+                }
+
+                function initStreetStatsArrayIfNecessary(districtName) {
+                    if (districtsStats[districtName].streetsStats === undefined) {
+                        districtsStats[districtName].streetsStats = {};
+                    }
+                }
+
+                function calculateStreetsStatsForDistrict(districtStats, districtName) {
+                    _.forIn(districtStats.streets, function (streetStats, streetName) {
+                        districtsStats[districtName].streetsStats[streetName] = makeStats(streetStats);
+                    });
+                }
+
+                _.forIn(districts, function (districtStats, districtName) {
+                    districtsStats[districtName] = makeStats(districtStats);
+                    initStreetStatsArrayIfNecessary(districtName);
+                    calculateStreetsStatsForDistrict(districtStats, districtName);
+                });
+
+                return districtsStats;
+            }
 
         }]
 );
--- frontend/app/scripts/directives/statistics-directive/statistics-directive.js
+++ frontend/app/scripts/directives/statistics-directive/statistics-directive.js
@@ -6,177 +6,33 @@ angular.module('propertyBrokerApp.directives')
             templateUrl: 'scripts/directives/statistics-directive/statistics-directive.html',
             restrict: 'E',
             scope: {
-                data: '='
+                districtsStats: '=data'
             },
-            controller: ['$scope', function ($scope) {
+            controller: ['$scope', 'lodash', function ($scope, _) {
 
-                $scope.$watch('data', function (newValue) {
-                    $scope.data = newValue;
+                $scope.$watch('districtsStats', function (newValue) {
+                    $scope.districtsStats = newValue;
                 }, false);
 
-                function getMinMaxPrice(boolMin, propertyA, propertyB) {
-                    var price;
-                    if (boolMin === true) {
-                        price = Math.min(parseInt(propertyA.price), parseInt(propertyB.price));
-                    } else {
-                        price = Math.max(parseInt(propertyA.price), parseInt(propertyB.price));
+                var rowsByDistricts = {};
+
+                $scope.isExpanded = function(key) {
+                    return _.has(rowsByDistricts, key) && rowsByDistricts[key].expanded;
+                };
+
+                $scope.expandRow = function(key) {
+                    function collapse() {
+                        rowsByDistricts[key].expanded = false;
                     }
 
-                    if (parseInt(propertyB.price) === parseInt(price)) {
-                        return propertyB;
-                    } else {
-                        return propertyA;
+                    function expand() {
+                        if (rowsByDistricts[key] === undefined)
+                            rowsByDistricts[key] = {};
+                        rowsByDistricts[key].expanded = true;
                     }
-                }
 
-                function sumValues(valueA,valueB){
-                    return  parseInt(valueA) + parseInt(valueB);
-                }
-
-                function divideValues(valueA,valueB){
-                    return  Math.round( parseInt(valueA) / parseInt(valueB));
-                }
-
-
-
-                function getDistrictStats(properties) {
-                    var pricesInDistricts = [];
-
-                    properties.forEach(function (property) {
-                        var isDistrictListed = false;
-
-                        pricesInDistricts.forEach(function (propByDistrict) {
-                            if (property.address.district === propByDistrict.district && !isDistrictListed) {
-                                propByDistrict.sumPrices =sumValues(propByDistrict.sumPrices,property.price);
-                                propByDistrict.sumArea = sumValues(propByDistrict.sumArea,property.area);
-                                propByDistrict.count = sumValues(propByDistrict.count,1);
-                                if(property.address.street !=null){
-                                    propByDistrict.mostExpensive = getMinMaxPrice(false, propByDistrict.mostExpensive, property);
-                                    propByDistrict.cheapest = getMinMaxPrice(true, propByDistrict.cheapest, property);
-                                }
-                                propByDistrict.properties.push(property);
-                                isDistrictListed = true;
-                            }
-                        });
-                        if (!isDistrictListed && property.address.street !=null ) {
-                            pricesInDistricts.push({
-                                district: property.address.district,
-                                sumPrices: parseInt(property.price),
-                                sumArea: parseInt(property.area),
-                                count: 1,
-                                mostExpensive: property,
-                                cheapest: property,
-                                properties: [property]
-                            });
-                        }
-
-
-                    });
-                    return pricesInDistricts;
-                }
-
-
-                function calculateDistrictsStats() {
-
-                    var pricesInDistricts = getDistrictStats($scope.data);
-                    var avgPricesInDistricts = [];
-
-                    pricesInDistricts.forEach(function (propByDistrict) {
-                        avgPricesInDistricts.push(
-                            {
-                                district: propByDistrict.district,
-                                avgPriceMSquare: divideValues(propByDistrict.sumPrices, propByDistrict.sumArea),
-                                count: propByDistrict.count,
-                                avgPrice: divideValues(propByDistrict.sumPrices,propByDistrict.count),
-                                mostExpensive: propByDistrict.mostExpensive,
-                                cheapest: propByDistrict.cheapest
-                            }
-                        );
-                    });
-                    return avgPricesInDistricts;
-                }
-
-                function getStreetStats() {
-
-                    var valueDistrictStreet = getDistrictStats($scope.data);
-                    var pricesAllDistricts = [];
-
-                    valueDistrictStreet.forEach(function (district) {
-
-                        var pricesAllStreets = [];
-
-                        district.properties.forEach(function (propertyStreet) {
-
-                            var isStreetListed = false;
-
-                            pricesAllStreets.forEach(function (propByDistrict) {
-                                if (propertyStreet.address.street === propByDistrict.street && !isStreetListed ) {
-                                    propByDistrict.sumPrices = sumValues(propByDistrict.sumPrices,propertyStreet.price);
-                                    propByDistrict.sumArea = sumValues(propByDistrict.sumArea,propertyStreet.area);
-                                    propByDistrict.count =sumValues(propByDistrict.count,1);
-                                    propByDistrict.mostExpensive = getMinMaxPrice(false, propByDistrict.mostExpensive, propertyStreet);
-                                    propByDistrict.cheapest = getMinMaxPrice(true, propByDistrict.cheapest, propertyStreet);
-                                    isStreetListed = true;
-                                }
-                            });
-
-                            if (!isStreetListed && propertyStreet.address.street!=null) {
-                                pricesAllStreets.push({
-                                    street: propertyStreet.address.street,
-                                    sumPrices: parseInt(propertyStreet.price),
-                                    sumArea: parseInt(propertyStreet.area),
-                                    count: 1,
-                                    mostExpensive: propertyStreet,
-                                    cheapest: propertyStreet
-                                });
-                                isStreetListed = true;
-                            }
-                        });
-
-                        pricesAllDistricts.push({
-                            district: district.district,
-                            streets: pricesAllStreets
-                        })
-                    });
-
-                    return pricesAllDistricts;
-                }
-
-
-                function calculateStreetStats() {
-
-                    var pricesInDistricts = getStreetStats($scope.data);
-                    var districtStatsByStreets = [];
-
-                    pricesInDistricts.forEach(function (district) {
-
-                        var streetsStat = []
-
-                        district.streets.forEach(function (propByStreet) {
-                            streetsStat.push(
-                                {
-                                    street: propByStreet.street,
-                                    avgPriceMSquare: divideValues(propByStreet.sumPrices,propByStreet.sumArea),
-                                    count: propByStreet.count,
-                                    avgPrice: divideValues(propByStreet.sumPrices,propByStreet.count),
-                                    mostExpensive: propByStreet.mostExpensive,
-                                    cheapest: propByStreet.cheapest
-                                }
-                            );
-                        });
-
-                        districtStatsByStreets.push({
-                            district: district.district,
-                            streets: streetsStat
-                        })
-                    });
-
-                    return districtStatsByStreets;
-                }
-
-                $scope.districtsStats = calculateDistrictsStats();
-                $scope.streetStats = calculateStreetStats();
-
+                    $scope.isExpanded(key) ? collapse() : expand();
+                };
 
             }]
         };
